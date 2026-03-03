1e508a817a58c6cd2605a37ffcd5f6af96e67ddf
Angela S. Milford <angela.milford123@gmail.com>
2026-03-03 11:43:28 -1100

    Made a few corrections for OAuth to work

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,8 @@ const mongodb = require('../db/connect');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    //callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   async (accessToken, refreshToken, profile, done) => {
     const db = mongodb.getDb().db();
