bd067ee395ec10d5ac0bd4dc6507de5f3f5b0525
Angela S. Milford <angela.milford123@gmail.com>
2026-03-03 11:35:57 -1100

    Made a few corrections regarding OAuth

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
 require('dotenv').config();
 require('./config/passport');
 
-
 // --------------------
 // Module dependencies
 // ---------------------
 const express = require('express');
 const cors = require('cors');
 const app = express();
-// const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3000;
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
-// const graphqlHTTP = require('express-graphql');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
-const { MongoDBNamespace } = require('mongodb');
 
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
@@ -26,19 +22,25 @@ const mongodb = require('./db/connect');
 const session = require('express-session');
 const passport = require('passport');
 
+// -----------
+// Middleware 
+// -----------
 
-// -----------
-// Middleware
-// -----------
+app.use(session({
+  secret: 'mySecretKey',
+  resave: false,
+  saveUninitialized: false
+}));
+
 app
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use(passport.initialize())
+  .use(passport.session())
   .use(cors())
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
-  .use('/', require('./routes'))
   .use(bodyParser.json())
-  .use(passport.initialize())
-  .use(passport.session());
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+  .use('/', require('./routes'));
 
 app.use('/graphql', graphqlHTTP({
   schema,
@@ -48,19 +50,13 @@ app.use('/graphql', graphqlHTTP({
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   next();
-})
-
-app.use(session({
-  secret: 'mySecretKey',
-  resave: false,
-  saveUninitialized: false
-}));
+});
 
 
 // --------------------------------
 process.on('uncaughtException', (err, origin) => {
   console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
-})
+});
 
 
 // --------------------
@@ -76,32 +72,3 @@ mongodb.initDb((err) => {
     });
   }
 });
-
-// const mongoose = require('mongoose');
-
-// mongoose.connect(process.env.MONGODB_URI);
-
-// mongoose.connection.once('open', () => {
-  // console.log('Connected to database');
-// });
-
-// const db = require('./models');
-// db.mongoose
-  // .connect(db.url, {
-    // useNewUrlParser: true,
-    // useUnifiedTopology: true,
-  // })
-  // .then(() => {
-    // console.log('Connected to the database!');
-  // })
-  // .catch((err) => {
-    // console.log('Cannot connect to the database!', err);
-    // process.exit();
-  // });
-
-
-
-// Server setup
-// app.listen(port, () => {
-  // console.log(`Server is running on port ${port}.`);
-// });
